d4f2cd4a933d8645f3a4235f41a589a2cab8b714
Thomas Schrauwen <114558568+stermon13@users.noreply.github.com>
2026-03-30 10:16:00 +0200

    keep running into errors

--- db/alter.sql
+++ db/alter.sql
@@ -717,21 +717,21 @@ CREATE TABLE
             FOREIGN KEY (documentGroupCId) REFERENCES DocumentGroupC (id) ON DELETE RESTRICT
       ) ENGINE = InnoDB;
 
-ALTER TABLE DocumentStructure ADD IF NOT EXISTS documentGroupAId CHAR(36) NOT NULL,
+ALTER TABLE DocumentStructure ADD COLUMN IF NOT EXISTS documentGroupAId CHAR(36) NOT NULL,
 DROP FOREIGN KEY IF EXISTS fk_documentStructure_documentGroupA,
-ADD CONSTRAINT fk_documentStructure_documentGroupA FOREIGN KEY (documentGroupAId) REFERENCES DocumentGroupA (id) ON DELETE RESTRICT;
-ALTER TABLE DocumentStructure ADD IF NOT EXISTS documentGroupBId CHAR(36) NULL,
+ADD CONSTRAINT IF NOT EXISTS fk_documentStructure_documentGroupA FOREIGN KEY (documentGroupAId) REFERENCES DocumentGroupA (id) ON DELETE RESTRICT;
+ALTER TABLE DocumentStructure ADD COLUMN IF NOT EXISTS documentGroupBId CHAR(36) NULL,
 DROP FOREIGN KEY IF EXISTS fk_documentStructure_documentGroupB,
-ADD CONSTRAINT fk_documentStructure_documentGroupB FOREIGN KEY (documentGroupBId) REFERENCES DocumentGroupB (id) ON DELETE RESTRICT;
-ALTER TABLE DocumentStructure ADD IF NOT EXISTS documentGroupCId CHAR(36) NULL,
+ADD CONSTRAINT IF NOT EXISTS fk_documentStructure_documentGroupB FOREIGN KEY (documentGroupBId) REFERENCES DocumentGroupB (id) ON DELETE RESTRICT;
+ALTER TABLE DocumentStructure ADD COLUMN IF NOT EXISTS documentGroupCId CHAR(36) NULL,
 DROP FOREIGN KEY IF EXISTS fk_documentStructure_documentGroupC,
-ADD CONSTRAINT fk_documentStructure_documentGroupC FOREIGN KEY (documentGroupCId) REFERENCES DocumentGroupC (id) ON DELETE RESTRICT;
-ALTER TABLE DocumentStructure ADD IF NOT EXISTS documentGroupDId CHAR(36) NULL,
+ADD CONSTRAINT IF NOT EXISTS fk_documentStructure_documentGroupC FOREIGN KEY (documentGroupCId) REFERENCES DocumentGroupC (id) ON DELETE RESTRICT;
+ALTER TABLE DocumentStructure ADD COLUMN IF NOT EXISTS documentGroupDId CHAR(36) NULL,
 DROP FOREIGN KEY IF EXISTS fk_documentStructure_documentGroupD,
-ADD CONSTRAINT fk_documentStructure_documentGroupD FOREIGN KEY (documentGroupDId) REFERENCES DocumentGroupD (id) ON DELETE RESTRICT;
-ALTER TABLE DocumentStructure ADD IF NOT EXISTS documentPlaceId CHAR(36) NOT NULL,
+ADD CONSTRAINT IF NOT EXISTS fk_documentStructure_documentGroupD FOREIGN KEY (documentGroupDId) REFERENCES DocumentGroupD (id) ON DELETE RESTRICT;
+ALTER TABLE DocumentStructure ADD COLUMN IF NOT EXISTS documentPlaceId CHAR(36) NOT NULL,
 DROP FOREIGN KEY IF EXISTS fk_documentStructure_documentPlace,
-ADD CONSTRAINT fk_documentStructure_documentPlace FOREIGN KEY (documentPlaceId) REFERENCES DocumentPlace (id) ON DELETE RESTRICT;
+ADD CONSTRAINT IF NOT EXISTS fk_documentStructure_documentPlace FOREIGN KEY (documentPlaceId) REFERENCES DocumentPlace (id) ON DELETE RESTRICT;
 
 ALTER TABLE MaterialSerialTrack CHANGE COLUMN IF EXISTS `serialTrackedId` `serialTrackedId` CHAR(36) NULL;
 SET @tbl_exists = (

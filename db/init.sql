c85d272c53ef5e8a33873dd0e6546630b63b8aa1
Thomas Schrauwen <114558568+stermon13@users.noreply.github.com>
2026-04-24 14:02:58 +0200

    init update

--- db/init.sql
+++ db/init.sql
@@ -2349,3 +2349,9 @@ CREATE TABLE
         FOREIGN KEY (employeeId) REFERENCES Employee (id) ON DELETE RESTRICT,
         FOREIGN KEY (projectId) REFERENCES Project (id) ON DELETE RESTRICT
     ) ENGINE = InnoDB;
+
+ALTER TABLE InvoiceOut ADD COLUMN IF NOT EXISTS boqId CHAR(36) NULL;
+
+ALTER TABLE InvoiceOut
+    ADD CONSTRAINT fk_invoiceout_boq
+    FOREIGN KEY (boqId) REFERENCES BillOfQuantities (id) ON DELETE RESTRICT;
